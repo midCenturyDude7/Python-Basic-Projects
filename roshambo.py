2578670ae90650bdacb70e3588000bb2ca58269f
midCenturyDude7 <mgriffes@gmail.com>
2020-08-19 21:12:43 -0700

    Updated the control flow and conditional statements

--- roshambo.py
+++ roshambo.py
@@ -6,6 +6,11 @@ Filename: roshambo.py
 Program is a two-player Rock-Paper-Scissors game
 """
 
+# Constants
+ROCK = 'Rock'
+PAPER = 'Paper'
+SCISSORS = 'Scissors'
+
 def main():
 
     # Introduction
@@ -17,18 +22,36 @@ def main():
         player_one = input("Player one: ")
         player_two = input("Player two: ")
 
-        # Assign values to strings
-        rock = ["Rock", "rock"]
-        paper = ["Paper", 'paper']
-        scissors = ["Scissors", "scissors"]
-
         # Assign weights to Rock, Paper and Scissors
-        if player_one == rock or paper or scissors and player_two == rock or paper or scissors:
-            if player_one == player_two:
-                print("Try again!")
-            else: print("That's not an option. You must enter, 'Rock', 'Paper' or 'Scissors'.")
+        if player_one == player_two:
+            print("You both entered the same weapon! Try again!")
+        elif player_one == ROCK and player_two == SCISSORS:
+            print("Player One is the winner!")
+            break
+        elif player_one == ROCK and player_two == PAPER:
+            print("Player Two is the winner!")
+            break
+        elif player_one == PAPER and player_two == ROCK:
+            print("Player One is the winner!")
+        elif player_one == PAPER and player_two == SCISSORS:
+            print("Player Two is the winner!")
+        elif player_one == SCISSORS and player_two == ROCK:
+            print("Player Two is the winner!")
+        elif player_one == SCISSORS and player_two == PAPER:
+            print("Player One is the winner!")
+        else:
+            print("That's not an option. You must enter, 'Rock', 'Paper' or 'Scissors'.") 
+            print("Try Again!")
 
-
+def play_again():
+    play_it = "Y"
+    end_game = "N"
+    user_input = input("Would you like to play again? Y/N: ")
+    
+    if user_input == end_game:
+        print("Thanks for playing!")
+    else:
+        print("Okay, let's play again!")
 
 
 if __name__ == '__main__':
