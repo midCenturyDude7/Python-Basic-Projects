3f04b566238dbe1bdbe32ec2b2f0105304b4662b
midCenturyDude7 <mgriffes@gmail.com>
2020-12-05 10:46:59 -0800

    User log for machines active on a network

---
+++ machine_user_log.py
@@ -0,0 +1,33 @@
+"""
+Filename: machine_user_log.py
+Program will check to see which users are actively logged on to a machine
+on the network and generate a report accordingly
+"""
+
+def get_event_date(event):
+    return event.date
+
+def current_users(events):
+    events.sort(key=get_event_date)
+    machines = {}
+    for event in events:
+        if event.machine not in machines:
+            machines[event.machine] = set()
+        if event.type == "login":
+            machines[event.machine].add(event.user)
+        elif event.type == "logout":
+            machines[event.machine].remove(event.user)
+    return machines
+
+def generate_report(machines):
+    for machine in machines.items():
+        if len(users) > 0:
+            user_list = ", ".join(users)
+            print("{}: {}".format(machine, user_list))
+
+class Event:
+    def __init__(self, event_date, event_type, machine_name, user):
+        self.date = event_date
+        self.type = event_type
+        self.machine = machine_name
+        self.user = user
